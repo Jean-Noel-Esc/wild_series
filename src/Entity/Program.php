5fa68f17654f9faa3b5c5f8d238d822d5394ad73
Jean-Noel-Esc <jeannoelescande@gmail>
2021-04-27 11:04:01 +0200

    13/symfony/Form/Validation/AddProgram

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -6,9 +6,12 @@ use App\Repository\ProgramRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=ProgramRepository::class)
+ * @UniqueEntity("title", message="Ce titre existe déjà")
  */
 class Program
 {
@@ -21,11 +24,14 @@ class Program
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Veuillez renseigner le titre.")
+     * @Assert\Length(max="255", maxMessage="Le titre de la série {{ value }} est trop long, il ne doit pas dépasser {{ limit }} caractères")
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank(message="Veuillez renseigner une description.")
      */
     private $summary;
 
